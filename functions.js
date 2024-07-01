fae65ec5b30a48e12211f5939f4e16e8f6893a13
YateetAgrawal001 <yateetagrawal@gmail.com>
2024-07-01 16:33:22 +0530

    functions ends here video 20

--- functions.js
+++ functions.js
@@ -22,5 +22,22 @@ function addTwoNumbers(number1, number2){
 addTwoNumbers(3,5)
 
 const result = addTwoNumbers(1,2)
-console.log(result) //in method second of defining the funct it will print result cause we are returning it
+//console.log(result) //in method second of defining the funct it will print result cause we are returning it
 
+const user = {
+    name: "yateet",
+    age: 29,
+    username:"Dio" 
+}
+function helpUser(anyobject){
+    console.log(`Name is ${anyobject.username} and age is ${anyobject.age}`)
+}
+helpUser(user)
+
+const newArray = [100,200,300,400,500];
+
+function returnvalueatIdx(anyarray){
+    return anyarray[2]
+}
+let element = returnvalueatIdx(newArray)
+console.log(element)
